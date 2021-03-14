a61fba905efe28edaab91d934ea16f9ac0bedaae
Nico Zerpa <nico@nicozerpa.com>
2021-03-14 14:24:38 -0300

    Fix matomo

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       resolve: "gatsby-plugin-matomo",
       options: {
         siteId: 1,
-        matomoUrl: "https://matomo.nicozerpa.com/",
+        matomoUrl: "https://matomo.nicozerpa.com",
         siteUrl: "https://nicozerpa.com"
       }
     }
