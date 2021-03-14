57debf3f644b1120cafc0542233c1d38240e7f95
Nico Zerpa <nico@nicozerpa.com>
2021-03-14 16:30:33 -0300

    Blog post: open links in new window

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,29 +1,43 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import NewsletterForm from "../components/newsletter-form"
 
 export default function BlogPost({ data }) {
-    const post = data.markdownRemark;
+    const post = data.markdownRemark
+
+    const articleBodyRef = useRef(null)
+
+    useEffect(() => {
+        if (articleBodyRef.current) {
+            articleBodyRef.current.querySelectorAll("a[href]").forEach(function(link) {
+                link.setAttribute("target", "_blank")
+            })
+        }
+    })
 
     return (
         <Layout title={ post.frontmatter.title }>
-          <article className="single">
-            <h1>{ post.frontmatter.title }</h1>
-            <div className="textContentWidth" dangerouslySetInnerHTML={{ __html: post.html }}></div>
-          </article>
-          <NewsletterForm/>
+            <article className="single">
+                <h1>{ post.frontmatter.title }</h1>
+                <div
+                    ref={ articleBodyRef }
+                    className="textContentWidth"
+                    dangerouslySetInnerHTML={{ __html: post.html }}>
+                </div>
+            </article>
+            <NewsletterForm/>
         </Layout>
     )
 }
 
 export const query = graphql`
-  query BlogQuery($slug: String!) {
+query BlogQuery($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
-      frontmatter {
-        title
-      }
+        html
+        frontmatter {
+            title
+        }
     }
-  }
+}
 `
