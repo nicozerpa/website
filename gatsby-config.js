c0cdc2f944234e93913d524fd21217835fb4af25
Nico Zerpa <nico@nicozerpa.com>
2021-03-28 23:43:36 -0300

    Sitemap fix to exclude unpublished pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
+    siteUrl: "https://nicozerpa.com",
     title: "Nico Zerpa, Your JavaScript Friend",
     description: "Whether you’re a beginner or advanced, I’ll help you level up your JavaScript skills"
   },
@@ -22,6 +23,42 @@ module.exports = {
     "gatsby-transformer-remark",
     "gatsby-plugin-sass",
     "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        query: `query{
+          allSitePage(
+            filter: {componentChunkName: {ne: "component---src-templates-blog-post-tsx"}}
+          ) {
+            nodes {
+              path
+            }
+          }
+          allMarkdownRemark(filter: {frontmatter: {published: {eq: 1}}}) {
+            nodes {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+        `,
+        resolveSiteUrl: () => `https://nicozerpa.com/`,
+        serialize: ({ allSitePage, allMarkdownRemark}) => {
+          
+          const basicPages = allSitePage.nodes.map(node => node.path)
+          const blogPages = allMarkdownRemark.nodes.map(node => node.fields.slug)
+
+          return basicPages.concat(blogPages).map(function (url) {
+            return {
+              url: `https://nicozerpa.com${url}`,
+              changefreq: "daily",
+              priority: 0.7
+            }
+          })
+        }
+      }
+    },
     {
       resolve: "gatsby-plugin-matomo",
       options: {
