055e3ae093a3d59ad8ee2346a6994bad2665ba19
Nico Zerpa <nico@nicozerpa.com>
2021-04-18 21:48:03 -0300

    Add Code Page

---
+++ src/pages/--code--.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react"
+import "../styles/codePage.scss"
+
+
+function updateCode(setCode : React.Dispatch<React.SetStateAction<string>>, element : HTMLPreElement) {
+    element.querySelectorAll<HTMLElement>("*").forEach((element : HTMLElement) : void => {
+        element.style.fontSize = "0.9rem"
+        element.style.lineHeight = "1.2em"
+    })
+    setCode(element.innerHTML)
+}
+
+export default function Code() : JSX.Element {
+    const [code, setCode] = useState<string>("")
+
+    return <div id="codePage">
+        <pre contentEditable={true} onInput={ (event) => updateCode(setCode, event.target as HTMLPreElement) }></pre>
+        <pre>{ code }</pre>
+    </div>
+}
