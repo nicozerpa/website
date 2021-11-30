1f74a31d02062cc493f06daa6b8a72642470b748
Nico Zerpa <nico@nicozerpa.com>
2021-11-30 20:19:27 -0300

    Migration: article list

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -1,34 +1,72 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-import Layout from "../components/layout"
+import React from "react";
+import Link from "next/link";
+import Layout from "../components/layout";
+import { remark } from "remark";
+import matter from "gray-matter";
+import html from "remark-html";
 
-
-interface ArticlesProps {
-    data: {
-        articles: {
-            posts: [
-                {
-                    frontmatter: {
-                        id: number,
-                        title: string
-                    },
-                    excerpt: string,
-                    fields: {
-                        slug: string
-                    }
-                }
-            ],
-            pageInfo: {
-                pageCount: number,
-                currentPage: number
-            }
-        }
-    }
+interface Post {
+    id: number,
+    title: string
+    description: string,
+    slug: string
 }
 
-export default function Articles({ data } : ArticlesProps) : JSX.Element {
-    const { posts, pageInfo } = data.articles
 
+
+export async function getServerSideProps() {
+
+    const fs = await import("fs");
+    const path = await import("path");
+
+    const articlesDir = path.join(process.cwd(), "src", "articles");
+
+    async function listArticles(): Promise<Post[]>
+    {
+        const output = new Array<Post>();
+        for (const file of fs.readdirSync(articlesDir)) {
+
+            if (file.trim().toLowerCase().match(/\.md$/)) {
+
+                const rawPost = fs.readFileSync(
+                    path.join(articlesDir, file)
+                ).toString();
+
+                const postMetaData = matter(rawPost).data as Post;
+                
+                postMetaData.slug = "/" + file.trim().toLowerCase().replace(/\.md$/, "");
+
+                output.push(postMetaData);
+            }
+        }
+
+        output.sort((a, b) => b.id - a.id);
+
+        return output;
+    }
+
+
+    const posts = await listArticles();
+    
+
+    // Pass data to the page via props
+    return {
+        props: {
+            posts
+        }
+    };
+}
+
+
+export default function Articles({ posts } : { posts: Post[] } ) : JSX.Element {
+    //const { posts, pageInfo } = data.articles
+
+    const pageInfo = {
+        pageCount: 1,
+        currentPage: 1
+    };
+
+    
     const paginationArray: React.ReactNode[] = [];
     let pagination : React.ReactNode
     
@@ -37,16 +75,16 @@ export default function Articles({ data } : ArticlesProps) : JSX.Element {
         const maxPage = Math.min(minPage + 4, pageInfo.pageCount);
         
         if (minPage > 1) {
-            paginationArray.push(<Link key="1" to="/articles/">«</Link>)
+            paginationArray.push(<Link key="1" href="/articles/">«</Link>)
         }
 
         for (let i = minPage; i <= maxPage; i++) {
             const queryString = i > 1 ? `${i}` : "";
-            paginationArray.push(<Link key={ i } to={ `/articles/${queryString}` }>{ i }</Link>)
+            paginationArray.push(<Link key={ i } href={ `/articles/${queryString}` }>{ i }</Link>)
         }
 
         if (maxPage < pageInfo.pageCount) {
-            paginationArray.push(<Link key={ pageInfo.pageCount } to={ `/articles/${pageInfo.pageCount}` }>»</Link>)
+            paginationArray.push(<Link key={ pageInfo.pageCount } href={ `/articles/${pageInfo.pageCount}` }>»</Link>)
         }
         
         pagination = <div className="pagination">{ paginationArray }</div>
@@ -57,9 +95,9 @@ export default function Articles({ data } : ArticlesProps) : JSX.Element {
             <h1>Articles</h1>
             <div className="textContentWidth">
                 { posts.map(post => (
-                    <article key={ post.frontmatter.id }>
-                        <h2><Link to={ post.fields.slug }>{ post.frontmatter.title }</Link></h2>
-                        <p>{ post.excerpt }</p>
+                    <article key={ post.id }>
+                        <h2><Link href={ post.slug }>{ post.title }</Link></h2>
+                        <p>{ post.description }</p>
                     </article>
                 ))}
                 { pagination }
@@ -68,7 +106,7 @@ export default function Articles({ data } : ArticlesProps) : JSX.Element {
     )
 }
 
-
+/*
 export const PageQuery = graphql`
 query ArticleListPosts($limit: Int!, $skip: Int!, ) {
     articles: allMarkdownRemark(
@@ -104,4 +142,4 @@ query ArticleListPosts($limit: Int!, $skip: Int!, ) {
         }
     }
 }  
-`
+`*/
